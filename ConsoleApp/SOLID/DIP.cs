5b8091339a8a7a9604f6217a7b086b6756cc6893
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-01-08 17:15:57 +0300

    1. gün tamamlandı.

---
+++ ConsoleApp/SOLID/DIP.cs
@@ -0,0 +1,80 @@
+﻿namespace ConsoleApp.SOLID;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Created { get; set; }
+}
+
+public class ProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Created { get; set; }
+}
+
+public class ProductRepositoryFactory
+
+{
+    public IProductRepository Create()
+    {
+        return new ProductRepositoryWithSqlServer();
+    }
+
+    public IProductRepository CreateOracle()
+    {
+        return new ProductRepositoryWithOracle();
+    }
+}
+
+public interface IProductRepository
+
+{
+    List<Product> GetAll();
+}
+
+public class ProductRepositoryWithOracle : IProductRepository
+{
+    public List<Product> GetAll()
+    {
+        return new List<Product>
+        {
+            new() { Id = 1, Name = "Kalem 1 Oracle", Created = DateTime.Now },
+            new() { Id = 2, Name = "Kalem 2 Oracle", Created = DateTime.Now }
+        };
+    }
+}
+
+public class ProductRepositoryWithSqlServer : IProductRepository
+{
+    public List<Product> GetAll()
+    {
+        return new List<Product>
+        {
+            new() { Id = 1, Name = "Kalem 1", Created = DateTime.Now },
+            new() { Id = 2, Name = "Kalem 2", Created = DateTime.Now }
+        };
+    }
+}
+
+public class ProductService
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+
+    public List<ProductDto> GetAll()
+    {
+        return _productRepository.GetAll().Select(x => new ProductDto
+        {
+            Id = x.Id,
+            Name = x.Name,
+            Created = x.Created.ToShortDateString()
+        }).ToList();
+    }
+}
---
+++ ConsoleApp/SOLID/LSP.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp.SOLID.LSP;
+
+public interface ITakePhoto
+{
+    void TakePhoto();
+}
+
+public abstract class Phone
+{
+    public void Call()
+    {
+        Console.WriteLine("Call to phone");
+    }
+
+    //public abstract void TakePhoto();
+}
+
+public class IPhone12 : Phone, ITakePhoto
+{
+    //public override void TakePhoto()
+    //{
+    //    Console.WriteLine("Take a photo");
+    //}
+    public void TakePhoto()
+    {
+        Console.WriteLine("Take a photo");
+    }
+}
+
+public class Nokia : Phone
+{
+    //public override void TakePhoto()
+    //{
+    //    throw new NotImplementedException();
+    //}
+}
